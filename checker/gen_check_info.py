e6859590e54714430fe086cc4f3246a5662374cb
Cyl18 <cyl18a@gmail.com>
2025-04-23 21:55:27 +0800

    update gen_check_info and add revyos_checker

--- checker/gen_check_info.py
+++ checker/gen_check_info.py
@@ -1,9 +1,117 @@
-from checker.lib.db_models import CheckInfoElement, CheckType
+from enum import Enum
+import json
+
+import requests
+from lib.db_models import CheckInfoElement, CheckType
 import tomli
+import os
+from dataclasses import dataclass
+import semver
+
+path = "..\\packages-index\\manifests\\board-image\\"
+
+@dataclass
+class DistFileInfo:
+    name: str
+    url: str
 
 
+def main():
+    # get all files in path
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        # if filenames count > 1 then filter the newest version file using semver
+        # if filenames count == 1 then add the file to files
+        if len(filenames) > 1:
+            # filter the filenames using semver
+            semver_filenames = []
+            for filename in filenames:
+                if filename.endswith(".toml"):
+                    semver_filenames.append(filename)
+            # sort the filenames using semver
+            semver_filenames.sort(key=lambda x: semver.VersionInfo.parse(x.split(".toml")[0]))
+            # get the newest version file
+            files.append(os.path.join(root, semver_filenames[-1]))
+        elif len(filenames) == 1:
+            for filename in filenames:
+                if filename.endswith(".toml"):
+                    files.append(os.path.join(root, filename))
 
-def parse_single_file(path):
+    check_info = []
+    
+    for file in files:
+        urls = parse_single_file(file)
+        for url in urls:
+            check_info.append(CheckInfoElement(name=url.name, check_path=url.url, check_type=filter_check_type(url.url)))
+    
+    # convert to json with indent=4
+    # check_type is CheckType, convert to str
+    
+    
+    accessible_count = 0
+    unknown_count = 0
+    unknown_and_accessible_count = 0
+    # for each check_info, check if url is accessible
+    for item in check_info:
+        if item.check_type == CheckType.UNKNOWN:
+            unknown_count += 1
+            if check_if_url_is_accessible(item.check_path):
+                unknown_and_accessible_count += 1
+                accessible_count += 1
+            else:
+                item.check_type = CheckType.UNKNOWN_AND_INACCESSIBLE
+            continue
+        if check_if_url_is_accessible(item.check_path):
+            accessible_count += 1
+    
+    json_str = json.dumps([{
+        "name": item.name,
+        "check_path": item.check_path,
+        "check_type": item.check_type.value
+    } for item in check_info], indent=4)
+    
+    with open("check_info.json", "w") as f:
+        f.write(json_str)
+
+    print(f"Total: {len(check_info)}, \nAccessible: {accessible_count}, \nUnknown: {unknown_count}, \nImplemented Rate: {(len(check_info)-unknown_count)/len(check_info) * 100:.1f}%, \nRequired Implemented Rate: {((1-(unknown_and_accessible_count/len(check_info))) * 100):.1f}%")
+
+def check_if_url_is_accessible(url: str) -> bool:
+    try:
+        response = requests.head(url, allow_redirects=True)
+        if response.status_code == 200:
+            return True
+        elif response.status_code in [301, 302]:
+            return True
+        else:
+            return False
+    except requests.RequestException as e:
+        print(f"Error accessing URL: {e}")
+        return False
+def filter_check_type(url: str):
+    # if startwith https://github.com
+    if url.startswith("https://github.com"):
+        return CheckType.GITHUB
+    elif url.startswith("https://mirrors.tuna.tsinghua.edu.cn/openwrt"):
+        return CheckType.OPENWRT
+    elif url.startswith("https://mirror.iscas.ac.cn/openeuler-sig-riscv/openEuler-RISC-V/preview/openEuler-23.09-V1-riscv64/lpi4a/"):
+        return CheckType.OPENEULER_LPI4A
+    elif url.startswith("https://mirror.iscas.ac.cn/revyos"):
+        return CheckType.REVYOS
+    else:
+        return CheckType.UNKNOWN
+    
+
+
+def filter_mirror(s: str) -> str:
+    if s.startswith("mirror://ruyi-3rdparty-canaan/"):
+        return s.replace("mirror://ruyi-3rdparty-canaan/", "https://mirror.iscas.ac.cn/ruyisdk/3rdparty/canaan/")
+    elif s.startswith("mirror://ruyi-3rdparty-milkv/"):
+        return s.replace("mirror://ruyi-3rdparty-milkv/", "https://mirror.iscas.ac.cn/ruyisdk/3rdparty/milkv/")
+    else:
+        return s
+
+
+def parse_single_file(path) -> DistFileInfo:
     with open(path, 'rb') as f:  # tomli requires binary mode
         data = f.read()
     
@@ -11,16 +119,18 @@ def parse_single_file(path):
     urls_result = []
     
     node = toml.get("distfiles", [])
+    desc = toml.get("metadata", {}).get("desc", None)
     for o in node:
+        name = o["name"]
         if "fetch_restriction" in o:
             fetch_restriction = o["fetch_restriction"]
             ver = fetch_restriction["params"]["version"]
             restricted_software_name = toml["metadata"]["desc"]
             assert restricted_software_name == "Kingsoft WPS Office", f"new restricted software has been added, not WPS, name: {restricted_software_name}"
-            urls_result.append(f"wps://{ver}")
+
+            urls_result.append(DistFileInfo(name=desc, url=f"wps://{ver}"))
             continue
         
-        name = o["name"]
         urls = o.get("urls", None)
         
         if urls is not None:
@@ -33,11 +143,11 @@ def parse_single_file(path):
                 else:
                     urls1 = [x for x in urls1 if "mirror" not in x]
                 assert original_length - len(urls1) == 1
-                urls_result.append(urls1[0])
+                urls_result.append(DistFileInfo(name=desc, url=urls1[0]))
             elif urls1:
-                urls_result.append(urls1[0])
+                urls_result.append(DistFileInfo(name=desc, url=filter_mirror(urls1[0])))
         else:
-            urls_result.append(name)
+            urls_result.append(DistFileInfo(name=desc, url=name))
     
     return urls_result
 
---
+++ checker/lib/revyos_checker.py
@@ -0,0 +1,70 @@
+import os
+import re
+from typing import List
+from bs4 import BeautifulSoup
+import requests
+import semver
+from lib.db_models import CheckInfoElement, CheckResultElement
+from lib.checker_base import CheckerBase
+from urllib.parse import urlparse
+
+class RevyOSChecker(CheckerBase):
+    def check(self, data: CheckInfoElement) -> CheckResultElement:
+        # keep for future use
+        # segments = data.check_path.split("/")
+        # filename = segments[-1]
+        # regex = re.compile(r'\d{8}')
+        # match = regex.search(filename)
+        # if not match:
+        #     return CheckResultElement(data.name, "", failed=True)
+        # version = match.group().replace("-", "")
+        
+        # Get the latest timestamp from the URL
+        versions = self.get_timestamps(data.check_path)
+        
+        # Sort the versions in descending order
+        versions.sort(reverse=True)
+        
+        # Get the latest version
+        latest_version = versions[0]
+        
+        return CheckResultElement(data.name, latest_version, failed=False)
+    
+    @staticmethod
+    def get_timestamps(url):
+        # url is https://mirror.iscas.ac.cn/revyos/extra/images/sg2042/20241230/revyos-pioneer-20241230-212249.img.zst
+        # truncate to https://mirror.iscas.ac.cn/revyos/extra/images/sg2042/
+
+        parsed_url = urlparse(url)
+    
+        # Split the path and truncate to the desired section
+        base_path = '/'.join(parsed_url.path.split('/')[:5])  # Keep first six segments of the path
+    
+        # Reconstruct the truncated URL
+        request_uri = f"{parsed_url.scheme}://{parsed_url.netloc}{base_path}/"
+
+        response = requests.get(request_uri)
+        html = response.text
+        
+        # Parse the HTML
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        # Select all table rows in the specified path
+        trs = soup.select("body > pre a")
+        
+        # Initialize an empty list to store the timestamps
+        timestamp_list = []
+        
+        # Process each row, skipping the first two rows
+        for tr in trs[1:]:
+            # Get the first column's anchor element and its title attribute
+            file_name = tr.get("href", "null")
+            
+            # Extract timestamp from filename using regex
+            regex = re.compile(r'\d{8}')
+            match = regex.search(file_name)
+            if match:
+                timestamp_list.append(match.group())
+        
+        return timestamp_list
+
