6b99f189fd639bba2b962405d73d84eccd01c36e
Cyl18 <cyl18a@gmail.com>
2025-04-24 19:33:30 +0800

    checker: add revyos checker

--- checker/lib/checker.py
+++ checker/lib/checker.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 from lib.github_checker import GitHubReleaseChecker
 from lib.openwrt_checker import OpenWrtChecker
 from lib.openeuler_lpi4a_checker import OpenEulerLpi4aChecker
+from lib.revyos_checker import RevyOSChecker
 from lib.db_models import *
 def check_all(datas: List[CheckInfoElement]) -> List[CheckResultElement]:
     results = []
@@ -20,6 +21,10 @@ def check_all(datas: List[CheckInfoElement]) -> List[CheckResultElement]:
             results.append(result)
         elif data.check_type == CheckType.OPENEULER_LPI4A:
             checker = OpenEulerLpi4aChecker()
+            result = checker.check(data)
+            results.append(result)
+        elif data.check_type == CheckType.REVYOS:
+            checker = RevyOSChecker()
             result = checker.check(data)
             results.append(result)
         elif False:  # Placeholder for additional conditions
