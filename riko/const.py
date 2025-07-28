0d4c3e155d4321edcc9b85871a79195d33571a66
weilinfox <caiweilin@iscas.ac.cn>
2025-07-28 19:01:27 +0800

    riko: setup ruyi
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ riko/const.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+from config import config
+
+basedir = Path(__file__).resolve().parent.parent
+"""
+project directory
+"""
+
+datadir = basedir / 'cache' if config.use_base_dir else Path('~/.cache/riko/').expanduser()
+"""
+data or cache files
+"""
+
+nvchecker_datadir = datadir / 'nvchecker'
+"""
+nvchecker files
+"""
+
+ruyi_datadir = datadir / 'ruyi'
+"""
+ruyi files
+"""
+
+ruyi_config_dir = ruyi_datadir / 'config'
+ruyi_data_dir = ruyi_datadir / 'local'
+ruyi_cache_dir = ruyi_datadir / 'cache'
+ruyi_state_dir = ruyi_datadir / 'state'
+
+
+riko_datadir = datadir / 'riko'
+"""
+riko files
+"""
+
+ruyi_config = '''
+[telemetry]
+mode = "local"
+'''
+"""
+ruyi config toml file
+"""
+
+ruyi_config_extra = '''
+[repo]
+remote = "https://mirror.iscas.ac.cn/git/ruyisdk/packages-index.git"
+''' if config.use_ruyi_iscas_repo else ""
+"""
+ruyi extra config
+"""
