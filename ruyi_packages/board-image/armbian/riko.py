d7ba173af32d76b5721e05042f3ec4f653f2a603
weilinfox <caiweilin@iscas.ac.cn>
2025-08-12 15:32:40 +0800

    riko: add armbian riko.py
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ ruyi_packages/board-image/armbian/riko.py
@@ -0,0 +1,42 @@
+import semver
+
+from typing import List, Dict, Tuple
+
+from riko.api import RikoPkg, GithubUpstream
+
+
+def rikoring(old_pkgs: List[RikoPkg], new_pkgs: List[RikoPkg]) -> None:
+
+    # First, get new toml, some items such as upstream_version will automatically set.
+    # NOTE that `old_pkgs` is read only, do not modify it to prevent unexpected result
+    # Note that this package only has one combo
+    new_toml: Dict = new_pkgs[0].get_manifest()[0]
+    upstream: GithubUpstream = new_pkgs[0].get_upstream()
+    upstream_version: str = new_pkgs[0].get_upstream_version()
+
+    assert upstream.source == "github"
+
+    # Second, generate new version
+    # Upstream version is ruyi version
+    new_pkgs[0].version = semver.Version.parse(upstream_version)
+
+    # Third, edit metadata.desc
+    new_toml["metadata"]["desc"] = (
+        new_toml["metadata"]["desc"].replace(old_pkgs[0].get_upstream_version(), new_pkgs[0].get_upstream_version()))
+
+    # Fourth, get file
+    assets = upstream.get_release_asserts(upstream_version)
+    files: List[Tuple[str, str]] = []
+    for asset in assets:
+        print(asset.name)
+        if "Star64_noble" in asset.name and asset.name[-1] == "z":
+            files.append((asset.name, asset.browser_download_url))
+
+    print(files)
+    assert len(files) == 1
+    new_toml["blob"]["distfiles"] = [files[0][0], ]
+    new_toml["distfiles"][0]["name"] = files[0][0]
+    new_toml["distfiles"][0]["urls"] = [files[0][1], ]
+
+    # finally, set flag to ask riko package new one
+    new_pkgs[0].set_manifest_ready()
