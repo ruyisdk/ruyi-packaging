88ee04cafa35fdd4cbcd62aff62c01992f82304b
weilinfox <caiweilin@iscas.ac.cn>
2025-08-13 23:44:19 +0800

    riko: new riko.py for package revyos-lpi4a
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- riko/upstreams/regex.py
+++ riko/upstreams/regex.py
@@ -1,8 +1,10 @@
+import http.client
 import logging
 import re
-import requests
+import urllib.parse
+import urllib.request
 
-from typing import List, ClassVar
+from typing import ClassVar, List, Tuple
 
 from .upstream import Upstream
 
@@ -23,46 +25,49 @@ class RegexUpstream(Upstream):
         self._text = ""
         self._asserts: List[str] = []
 
+    def _file_name_and_url(self, f: str) -> Tuple[str, str]:
+        return f, urllib.parse.urljoin(self._file_url, f)
 
     def get_release_asserts(self) -> List[str]:
         if self._ready:
             return self._asserts
 
-        resp = requests.get(self._file_url)
-        if resp.status_code != 200:
-            raise RuntimeError(f"url {self._file_url} returned status code {resp.status_code}")
+        resp: http.client.HTTPResponse = urllib.request.urlopen(self._file_url, timeout=30.0)
+        if resp.status != http.client.OK:
+            raise RuntimeError(f"url {self._file_url} returned status code {resp.status}: "
+                               f"{http.client.responses[resp.status]}")
 
-        self._text = resp.text
+        self._text = resp.read().decode()
         self._asserts = re.findall(self._file_regex, self._text)
         self._ready = True
 
         return self._asserts
 
-    def get_release_asserts_substring(self, substr: str) -> List[str]:
+    def get_release_asserts_substring(self, substr: str) -> List[Tuple[str, str]]:
         r = []
 
         for f in self.get_release_asserts():
             if substr in f:
-                r.append(f)
+                r.append(self._file_name_and_url(f))
 
         return r
 
-    def get_release_assert_substring(self, substr: str) -> str:
+    def get_release_assert_substring(self, substr: str) -> Tuple[str, str]:
         r = self.get_release_asserts_substring(substr)
 
         assert len(r) == 1
         return r[0]
 
-    def get_release_asserts_regex(self, pattern: str) -> List[str]:
+    def get_release_asserts_regex(self, pattern: str) -> List[Tuple[str, str]]:
         r = []
 
         for f in self.get_release_asserts():
             if re.match(pattern, f):
-                r.append(f)
+                r.append(self._file_name_and_url(f))
 
         return r
 
-    def get_release_assert_regex(self, pattern: str) -> str:
+    def get_release_assert_regex(self, pattern: str) -> Tuple[str, str]:
         r = self.get_release_asserts_regex(pattern)
 
         assert len(r) == 1
