184a99b04a21969b7c577f8ac8e126b02e3d92b1
weilinfox <caiweilin@iscas.ac.cn>
2025-09-17 16:45:05 +0800

    ruyi_packages: add openbsd
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ ruyi_packages/board-image/openbsd/riko.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from riko.api import RikoPkg
+
+
+def rikoring(old_pkgs: List[RikoPkg], new_pkgs: List[RikoPkg]) -> None:
+    """
+    old toml stored in old_pkg, format new toml to new_pkg
+    :param old_pkgs: old pkg list
+    :param new_pkgs: new pkg list
+    :return:
+    """
+    urls = new_pkgs[0].get_manifest()[0]["distfiles"][0]["urls"]
+    urls.append(urls[0].replace("mirrors.tuna.tsinghua.edu.cn/OpenBSD", "cdn.openbsd.org/pub/OpenBSD"))
+    urls.append(urls[0].replace("mirrors.tuna.tsinghua.edu.cn/OpenBSD", "ftp.openbsd.org/pub/OpenBSD"))
