56620dadcbaee37eb5619f02e3922d2343bde5c8
weilinfox <caiweilin@iscas.ac.cn>
2025-10-30 13:28:53 +0800

    ruyi_packages: update revyos-sg2042 to latest format
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- ruyi_packages/board-image/revyos-sg2042/riko.py
+++ ruyi_packages/board-image/revyos-sg2042/riko.py
@@ -3,7 +3,7 @@ from typing import List
 from riko.api import RikoPkg, GithubUpstream
 
 
-def rikoring(old_pkgs: List[RikoPkg], new_pkgs: List[RikoPkg]) -> None:
+def post_rikoring(old_pkgs: List[RikoPkg], new_pkgs: List[RikoPkg]) -> None:
     """
     old toml stored in old_pkg, format new toml to new_pkg
     :param old_pkgs: old pkg list
@@ -11,19 +11,5 @@ def rikoring(old_pkgs: List[RikoPkg], new_pkgs: List[RikoPkg]) -> None:
     :return:
     """
 
-    upstream: GithubUpstream = new_pkgs[0].get_upstream()
-    upstream_version: str = new_pkgs[0].get_upstream_version()
-    assert upstream.source == "regex"
-
-    img, img_url = upstream.get_release_assert_regex(fr"revyos-pioneer-{upstream_version}-\d+\.img\.zst")
-
-    new_pkgs[0].version = old_pkgs[0].version.replace(minor=upstream_version, patch=0)
-
-    new_toml = new_pkgs[0].get_manifest()[0]
-    new_toml["metadata"]["desc"] = (new_toml["metadata"]["desc"].replace(old_pkgs[0].get_upstream_version(),
-                                                                         new_pkgs[0].get_upstream_version()))
-    new_toml["blob"]["distfiles"] = [img]
-    new_toml["distfiles"][0]["name"] = img
-    new_toml["distfiles"][0]["urls"] = [img_url]
-
-    new_pkgs[0].set_manifest_ready()
+    urls = new_pkgs[0].get_manifest()[0]["distfiles"][0]["urls"]
+    urls[0] = urls[0].replace("https://mirror.iscas.ac.cn/revyos/", "mirror://revyos/")
