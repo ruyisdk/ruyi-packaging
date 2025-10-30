741b16c2fe16e2798989f8a1a63df6fd5bfe427d
weilinfox <caiweilin@iscas.ac.cn>
2025-10-30 17:02:33 +0800

    ruyi_packages: new package revyos-laptop4a
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ ruyi_packages/board-image/revyos-laptop4a/riko.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from riko.api import RikoPkg, RegexUpstream
+
+
+def post_rikoring(old_pkgs: List[RikoPkg], new_pkgs: List[RikoPkg]) -> None:
+    """
+    old toml stored in old_pkg, format new toml to new_pkg
+    :param old_pkgs: old pkg list
+    :param new_pkgs: new pkg list
+    :return:
+    """
+
+    for p in new_pkgs:
+        for d in p.get_manifest()[0]["distfiles"]:
+            urls = d["urls"]
+            urls[0] = urls[0].replace("https://mirror.iscas.ac.cn/revyos/", "mirror://revyos/")
